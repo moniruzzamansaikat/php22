706070fc1d556464466dfb8dfb1a95ccca241506
moniruzzamansaikat <saikat.moniruzzaman@gmail.com>
2024-11-23 23:30:55 +0600

    custom headers

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -39,18 +39,24 @@ abstract class BaseController
         header("Location: $url");
         exit;
     }
-    
+
     /**
      * Return a JSON response.
      *
      * @param array $data The data to return as JSON.
      * @param int $statusCode The HTTP status code (default: 200).
+     * @param array $headers Custom header for json response
      * @return void
      */
-    protected function json(array $data, int $statusCode = 200)
+    protected function json(array $data, int $statusCode = 200, array $headers = [])
     {
         header('Content-Type: application/json');
         http_response_code($statusCode);
+
+        foreach ($headers as $key => $value) {
+            header("$key: $value");
+        }
+
         echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
         exit;
     }
